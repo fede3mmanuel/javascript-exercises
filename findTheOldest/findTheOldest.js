a599fc9de69d0686328f0fe5065b129d42cdb459
fede3mmanuel <fede@fede.website>
2020-06-10 13:57:42 -0500

    finTheOldest Works

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,32 @@
-let findTheOldest = function() {
+let findTheOldest = function(data) {
 
+    let name = "";
+    let age = 0;
+    let positonOnArray = -1;
+    let AnswerPosition = 0;
+
+    data.forEach(person => {
+        positonOnArray += 1;
+
+        if(person.yearOfDeath == undefined) {
+            let actualDate = new Date();
+            let actualYear = actualDate.getFullYear();
+            person.yearOfDeath = actualYear;
+                        
+        }
+
+        let newAge = person.yearOfDeath - person.yearOfBirth;        
+
+        if (newAge > age) {
+            name = person.name;
+            age = newAge;
+            AnswerPosition = positonOnArray;
+        }
+        
+    });
+    
+    return data[AnswerPosition];
+    
 }
 
 module.exports = findTheOldest
