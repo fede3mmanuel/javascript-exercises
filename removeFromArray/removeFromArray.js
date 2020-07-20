b974434177af4d672fa840bfa16be71b2dee1e70
fede3mmanuel <fede@fede.website>
2020-07-20 12:45:23 -0500

    removeFromArray finished
    Signed-off-by: fede3mmanuel <fede@fede.website>

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(completeArray, ...args) {
 
+    let finalArray = completeArray;
+    
+    for (let x = 0; x < args.length; x++) {
+
+        for (let i = 0; i < completeArray.length; i++) {
+
+            if (!isNaN(args[x]) && (typeof(args[x]) != "number")) {
+
+            } else {
+
+                if (completeArray[i] == args[x]) {
+
+                    finalArray.splice(i, 1);
+
+                }
+            }
+
+            
+            
+        }
+        
+    }
+
+    return finalArray;
 }
 
 module.exports = removeFromArray
