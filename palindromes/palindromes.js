4b1bcb5e3d1b8e042e28e75b29431d3a62f8d502
fede3mmanuel <fede@fede.website>
2020-07-20 10:19:54 -0500

    palindrome finished
    Signed-off-by: fede3mmanuel <fede@fede.website>

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function() {
+const palindromes = function(word) {
 
+    let result = true;
+
+    let regexFormWord = /[a-z]/gi;
+    let onlyLetters = word.match(regexFormWord);
+
+
+    for (let i = 0; i < onlyLetters.length; i++) {
+        
+        let left = (onlyLetters[i]);
+        let right = (onlyLetters[(onlyLetters.length - i) - 1]);
+        
+        if (left.toUpperCase() != right.toUpperCase()) {
+            return false;
+        }
+
+    }
+
+    return result;
 }
 
 module.exports = palindromes
