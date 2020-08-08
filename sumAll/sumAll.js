ddfd585f3887c5294738d387dc3a9c1df171751d
fede3mmanuel <fede@fede.website>
2020-08-08 18:11:00 -0500

    sumAll finished

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,4 +1,37 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+
+    if (firstNum < 1 || secondNum < 1) {
+        return "ERROR";
+    }
+
+    if (typeof(firstNum) != 'number' || typeof(secondNum) != 'number') {
+        return "ERROR";
+    }
+
+    let startNum = 0;
+    let endNum = 0;
+
+    if (firstNum < secondNum) {
+    
+        startNum = firstNum;
+        endNum = secondNum;
+    
+    } else {
+    
+        startNum = secondNum;
+        endNum = firstNum;
+    
+    }
+
+    let result = 0;
+    
+    for (let i = startNum; i <= endNum; i++) {
+        
+        result += i;
+    
+    }
+
+    return result;
 
 }
 
