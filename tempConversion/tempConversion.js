23ee4a1b89b6c34a428de0df904838b748a195d4
fede3mmanuel <fede@fede.website>
2020-08-08 19:03:51 -0500

    Finished tempConversion && finished the exercises :)

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,26 @@
-const ftoc = function() {
+const ftoc = function(Fnumber) {
+
+  let result = (Fnumber - 32) * (5/9);
+
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1))
+  }
+  
 
 }
 
+const ctof = function(Cnumber) {
 
-const ctof = function() {
+  let result = (Cnumber * (9/5)) + 32;
 
+  if (Number.isInteger(result)) {
+    return result;
+  } else {
+    return Number(result.toFixed(1))
+  }
+  
 }
 
 module.exports = {
